c7928a3293bc51a4c41725f9247485d90e2d715b
Adriaan van Rossum <git@adriaan.io>
2021-11-22 10:45:58 +0100

    Add NodeList for IE11

--- src/index-legacy.js
+++ src/index-legacy.js
@@ -4,4 +4,10 @@ import "whatwg-fetch";
 // Dummy call to make it work with a Polyfill in IE11
 Promise.resolve();
 
+// This polyfill adds compatibility to all browsers supporting ES5
+// https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach#polyfill
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 import "./index";
